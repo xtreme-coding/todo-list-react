120ff6f54f83026495e7f17a16cbb39a0362e50f
Soubhagya <soubhagyaranjanghs@gmail.com>
2021-02-03 17:49:35 +0530

    added strike through feature

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ToDoItem(props) {
-    return <li>{props.text}</li>
+
+    const [isDone, setIsDone] = useState(false);
+
+    function handleClick() {
+        setIsDone((prevValue) => {
+            return !prevValue;
+        })
+    }
+
+    return <div onClick={handleClick}>
+    <li style={{textDecoration: isDone ? "line-through" : "none"}}>{props.text}</li>
+    </div>
 }
 
 export default ToDoItem;
