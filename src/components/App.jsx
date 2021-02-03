17f4d6e457d05f617d3ff13450fa74943b6026fd
Soubhagya <soubhagyaranjanghs@gmail.com>
2021-02-03 17:38:20 +0530

    added todo item component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import ToDoItem from "./ToDoItem";
 
 function App() {
 
@@ -30,7 +31,10 @@ function App() {
       </div>
       <div>
         <ul>
-          {items.map((todoItem) => <li>{todoItem}</li>)}
+          {items.map((todoItem) => 
+            <ToDoItem
+              text={todoItem}
+             /> )}
         </ul>
       </div>
     </div>
