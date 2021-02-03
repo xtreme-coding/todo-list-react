ef0ae352ad5f0a9d2d93dcf8f4f4f61e473f24df
Soubhagya <soubhagyaranjanghs@gmail.com>
2021-02-03 18:57:56 +0530

    final bug fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,19 +4,12 @@ import InputArea from "./InputArea";
 
 function App() {
 
-    const [inputText, setInputText] = useState("");
     const [items, setItems] = useState([]);
 
-    function handleChange(event) {
-        const newValue = event.target.value;
-        setInputText(newValue);
-    }
-
-    function addItem() {
+    function addItem(inputText) {
         setItems((prevItems) => {
             return [...prevItems, inputText]
         })
-        setInputText("");
     }
 
     function deleteItem(id) {
@@ -32,7 +25,9 @@ function App() {
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      <InputArea />
+      <InputArea
+        onAdd={addItem}
+       />
       <div>
         <ul>
           {items.map((todoItem, index) => 
