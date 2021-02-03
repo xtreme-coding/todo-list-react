28863ddcfeed44a3dd964fd1d8a2491dcdb8f0a6
Soubhagya <soubhagyaranjanghs@gmail.com>
2021-02-03 08:56:26 +0530

    added add item function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,17 @@ import React, { useState } from "react";
 function App() {
 
     const [inputText, setInputText] = useState("");
+    const [items, setItems] = useState([]);
 
     function handleChange(event) {
         const newValue = event.target.value;
         setInputText(newValue);
+    }
+
+    function addItem() {
+        setItems((prevItems) => {
+            return [...prevItems, inputText]
+        })
     }
 
     return (
@@ -16,13 +23,13 @@ function App() {
       </div>
       <div className="form">
         <input onChange={handleChange} type="text" value={inputText} />
-        <button>
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {items.map((todoItem) => <li>{todoItem}</li>})
         </ul>
       </div>
     </div>
