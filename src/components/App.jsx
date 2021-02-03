0add857942c8baa3d9674ade9bedd0c2adcede51
Soubhagya <soubhagyaranjanghs@gmail.com>
2021-02-03 18:31:20 +0530

    added input area component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea";
 
 function App() {
 
@@ -31,12 +32,7 @@ function App() {
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      <div className="form">
-        <input onChange={handleChange} type="text" value={inputText} />
-        <button onClick={addItem}>
-          <span>Add</span>
-        </button>
-      </div>
+      <InputArea />
       <div>
         <ul>
           {items.map((todoItem, index) => 
